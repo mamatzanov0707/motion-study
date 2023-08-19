7f9a95e5ef985ec68ca7eee9833abf60dda08c2a
aizat1522 <aizat.amanbekova.m@gmail.com>
2023-08-19 18:38:55 +0600

    aizatm

--- src/App.js
+++ src/App.js
@@ -15,27 +15,27 @@ import HeroPage from "./components/HeroPage/HeroPage";
 import Submit from "./components/submit/submit";
 import English from "./Page/Hero/English/English";
 
-function App() {
-  return (
-    <div className="App">
-      <Header/>
-      <Routes>
-        <Route path={'/'} element={ <HeroPage/> }/>
-        <Route path={'/aboutUs'} element={ <AboutUs/> }/>
-        <Route path={'/studyAbroad'} element={ <Study/> }/>
-        <Route path={'/contacts'} element={ <Contact/> }/>
-        <Route path={'/flag'} element={ <FlagCard/> }/>
-        <Route path={'/country'} element={ <Country/> }/>
-        <Route path={'/other'} element={ <Other/>}/>
-        <Route path={'/items'} element={<Items/>}/>
-        <Route path={'/artis'} element={<Artis/>}/>
-        <Route path={'/cambridge'} element={<English/>}/>
-      </Routes>
+// function App() {
+//   return (
+//     <div className="App">
+//       <Header/>
+//       <Routes>
+//         <Route path={'/'} element={ <HeroPage/> }/>
+//         <Route path={'/aboutUs'} element={ <AboutUs/> }/>
+//         <Route path={'/studyAbroad'} element={ <Study/> }/>
+//         <Route path={'/contacts'} element={ <Contact/> }/>
+//         <Route path={'/flag'} element={ <FlagCard/> }/>
+//         <Route path={'/country'} element={ <Country/> }/>
+//         <Route path={'/other'} element={ <Other/>}/>
+//         <Route path={'/items'} element={<Items/>}/>
+//         <Route path={'/artis'} element={<Artis/>}/>
+//         <Route path={'/cambridge'} element={<English/>}/>
+//       </Routes>
 
 
-      <Footer/>
-    </div>
-  );
+//       <Footer/>
+//     </div>
+//   );
 import {useEffect, useState} from "react";
 import Loading from "./components/Loading/Loading";
 
@@ -68,9 +68,9 @@ function App() {
                         <Route path={'/other'} element={<Other/>}/>
                         <Route path={'/itams'} element={<Items/>}/>
                         <Route path={'/artis'} element={<Artis/>}/>
-
+                         <Route path={'/cambridge'} element={<English/>}/>
                     </Routes>
-                    <Submit/>
+                     {/* <Submit/> */}
                     <Footer/>
                 </div>
             </div>
