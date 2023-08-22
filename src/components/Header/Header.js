52defccf980fea61dbc561503161ad2ea0276e5f
Aizat1522 <126472059+Aizat1522@users.noreply.github.com>
2023-08-22 19:14:18 +0600

    aizattyky (#31)

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -18,6 +18,7 @@ const Header = () => {
     const [isBurgerOpen, setIsBurgerOpen] = useState(false); // Состояние для бургер-меню
     const [windowWidth, setWindowWidth] = useState(window.innerWidth);
     const [activeBurgerMenuItem, setActiveBurgerMenuItem] = useState(null);
+    const [redDotPosition, setRedDotPosition] = useState(0);
     const [searchCard , setSearchCard] = useState(false)
     const [value,setValue] = useState('1')
     const nav = useNavigate()
@@ -43,6 +44,33 @@ const Header = () => {
     console.log(filter)
 
     return (
+        <div id="header">
+            <div className="container">
+                <div className="header">
+                    <div className="header--title">
+                        <img src={logo1} alt=""/>
+                    </div>
+                    <div style={{display: dashed1 === 1 ? 'block' : 'none'}} className="red"></div>
+                    <div style={{display: dashed1 === 2 ? 'block' : 'none'}} className="red1"></div>
+                    <div style={{display: dashed1 === 3 ? 'block' : 'none'}} className="red2"></div>
+                    <div style={{display: dashed1 === 4 ? 'block' : 'none'}} className="red3"></div>
+
+                    {windowWidth <= 768 ? ( // Если ширина окна <= 768, показываем бургер-меню
+                    <div className="header--burger" onClick={() => setIsBurgerOpen(!isBurgerOpen)}>
+                        {isBurgerOpen ? <GrFormClose style={{zIndex:"100"}}/> : <RiMenu3Fill style={{zIndex:"100"}}/>}
+                    </div>
+                ) : (
+
+                    <div className="header--nav" >
+                        <div className="header--nav__navs">
+                            <NavLink to="/">Home</NavLink></div>
+                        <div className="header--nav__about">
+                            <NavLink to="/aboutUs">AboutUs</NavLink></div>
+                        <div className="header--nav__study">
+                            <NavLink to="/studyAbroad">Study Abroad</NavLink>
+                        </div>
+                        <div className="header--nav__contact">
+                            <NavLink to="/contacts">Contacts</NavLink>
         <>
             <div id="header">
                 <div className="container">
@@ -55,6 +83,48 @@ const Header = () => {
                         <div style={{display: dashed1 === 3 ? 'block' : 'none'}} className="red2"></div>
                         <div style={{display: dashed1 === 4 ? 'block' : 'none'}} className="red3"></div>
 
+                        <div className="header--nav1" style={{ display: isBurgerOpen ? 'block' : 'none' }}>
+                    <div className="header--nav1__navs1" onClick={()=> setIsBurgerOpen(false)}>
+    <NavLink to="/" onClick={() => setRedDotPosition(1)}>
+        Home {redDotPosition === 1 && <div className="red-dot"></div>}
+    </NavLink>
+</div>
+<div className="header--nav1__about" onClick={()=> setIsBurgerOpen(false)}>
+    <NavLink to="/aboutUs" onClick={() => setRedDotPosition(2)}>
+        AboutUs {redDotPosition === 2 && <div className="red-dot"></div>}
+    </NavLink>
+</div>
+<div className="header--nav1__study" onClick={()=> setIsBurgerOpen(false)}>
+    <NavLink to="/studyAbroad" onClick={() => setRedDotPosition(3)}>
+        Study Abroad {redDotPosition === 3 && <div className="red-dot"></div>}
+    </NavLink>
+</div>
+<div className="header--nav1__contact" onClick={()=> setIsBurgerOpen(false)}>
+    <a onClick={() => {
+        setRedDotPosition(4);
+        window.scroll(0, 3100);
+    }}>
+        Contacts {redDotPosition === 4 && <div className="red-dot"></div>}
+    </a>
+</div>
+                            {/* <div className="header--nav1__language1">
+                            <NavLink to="/contacts">Contacts</NavLink>
+                        </div> */}
+                       <div className="header--nav1__language1">
+                                <h3>EN</h3>
+                                <h3>RU</h3>
+                                <h3>KG</h3>
+                            </div>
+                        
+                       <div className="header--nav1__icon">
+                        <h3><FaTelegramPlane/></h3>
+                        <h3><AiFillInstagram/></h3>
+                        <h3><BsTelephoneXFill/></h3>
+                        <h3><MdEmail/></h3>
+                       </div>
+                    </div>
+                    )
+                   }
                         {windowWidth <= 768 ? ( // Если ширина окна <= 768, показываем бургер-меню
                             <div className="header--burger" onClick={() => setIsBurgerOpen(!isBurgerOpen)}>
                                 {isBurgerOpen ? <GrFormClose style={{zIndex: "100"}}/> :
